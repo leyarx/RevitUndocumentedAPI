45eec9944d4d2293ae0022366f27f1555a457650
Yaroslav Levandovskiy <leyarx@gmail.com>
2023-07-26 19:17:42 +0300

    initial commit

---
+++ RevitUndocumentedAPI/RevitUndocumentedExtensions.cs
@@ -0,0 +1,75 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Reflection;
+
+
+namespace RevitUndocumentedAPI
+{
+	public static class RevitUndocumentedExtensions
+	{
+        public static IList<RibbonItem> AddLabeledStackedItems(this RibbonPanel ribbonPanel, string labelName, RibbonItemData item1, RibbonItemData item2 = null)
+        {
+			if(labelName == null)
+            {
+				throw new ArgumentNullException(nameof(labelName));
+			}
+			if (item1 == null)
+			{
+				throw new ArgumentNullException(nameof(item1));
+			}
+
+			Type rRibbonPanelType = typeof(RibbonPanel);
+			MethodInfo canItemBeAddedToRowPanelMethod = 
+				rRibbonPanelType.GetMethod("canItemBeAddedToRowPanel", BindingFlags.NonPublic | BindingFlags.Instance);
+
+			if (!(canItemBeAddedToRowPanelMethod.Invoke(ribbonPanel, new object[] { item1 }) as bool?) ?? false)
+			{
+				string text = " is not supported as a stacked item.";
+				string name = item1.Name;
+				string text2 = "Item " + name;
+				throw new ArgumentException(text2 + text, nameof(item1));
+			}
+
+			if (item2 != null && (!(canItemBeAddedToRowPanelMethod.Invoke(ribbonPanel, new object[] { item2 }) as bool?) ?? false))
+			{
+				string text = " is not supported as a stacked item.";
+				string name = item2.Name;
+				string text2 = "Item " + name;
+				throw new ArgumentException(text2 + text, nameof(item2));
+			}
+
+			Autodesk.Windows.RibbonRowPanel ribbonRowPanel = new Autodesk.Windows.RibbonRowPanel();
+			Collection<Autodesk.Windows.RibbonItem> items = ribbonRowPanel.Items;
+
+			Autodesk.Windows.RibbonLabel label = new Autodesk.Windows.RibbonLabel() { Text = labelName, Height = 23 };
+			items.Add(label);
+			Autodesk.Windows.RibbonRowBreak ribbonRowBreak = new Autodesk.Windows.RibbonRowBreak();
+			items.Add(ribbonRowBreak);
+
+			List<RibbonItem> list = new List<RibbonItem>();
+
+			MethodInfo addItemToRowPanelMethod =
+				rRibbonPanelType.GetMethod("addItemToRowPanel", BindingFlags.NonPublic | BindingFlags.Instance);
+
+			RibbonItem rItem1 = addItemToRowPanelMethod.Invoke(ribbonPanel, new object[] { ribbonRowPanel, item1 }) as RibbonItem;
+			list.Add(rItem1);
+
+			if(item2 != null)
+            {
+				Autodesk.Windows.RibbonRowBreak ribbonRowBreak2 = new Autodesk.Windows.RibbonRowBreak();
+				items.Add(ribbonRowBreak2);
+
+				RibbonItem rItem2 = addItemToRowPanelMethod.Invoke(ribbonPanel, new object[] { ribbonRowPanel, item2 }) as RibbonItem;
+				list.Add(rItem2);
+			}
+
+			FieldInfo aRibbonPanelField = rRibbonPanelType.GetField("m_RibbonPanel", BindingFlags.NonPublic | BindingFlags.Instance);
+			Autodesk.Windows.RibbonPanel aRibbonPanel = aRibbonPanelField.GetValue(ribbonPanel) as Autodesk.Windows.RibbonPanel;
+
+			aRibbonPanel.Source.Items.Add(ribbonRowPanel);
+			return list;
+		}
+	}
+}
---
+++ RevitUndocumentedAPI/RibbonLabelCommand.cs
@@ -0,0 +1,43 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+//using Autodesk.Windows;
+
+namespace RevitUndocumentedAPI
+{
+    [Transaction(TransactionMode.Manual)]
+    public class RibbonLabelCommand : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            string tabName = "Undocumented";
+            string panelName = "Panel";
+
+            var uiapp = commandData.Application;
+
+            try
+            {
+                uiapp.CreateRibbonTab(tabName);
+            }
+            catch (Exception) { }
+
+            RibbonPanel panel = uiapp.GetRibbonPanels(tabName).FirstOrDefault(p => p.Name == panelName);
+
+            if (panel == null)
+            {
+                panel = uiapp.CreateRibbonPanel(tabName, panelName);
+            }
+
+            ComboBoxData comboBoxDataLevel = new ComboBoxData("LevelsSelector");
+            var sItems = panel.AddLabeledStackedItems("Active Workset:", comboBoxDataLevel);
+
+            return Result.Succeeded;
+        }
+    }
+}
